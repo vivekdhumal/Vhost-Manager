c083973c18485e52930c63f24a6acc15f8c7c6d8
Vivek Dhumal <dhumal.vivek02@gmail.com>
2017-12-21 15:55:37 +0530

    added config

---
+++ src/config.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /**
+     * This apache httpd-vhosts.conf file path.
+     */
+    'apache_host_path' => 'D:\wamp\bin\apache\apache2.4.9\conf\extra\httpd-vhosts.conf',
+    /**
+     * Your windows hosts file path, generaly it'll remain same.
+     */
+    'windows_host_path' => 'C:\Windows\System32\drivers\etc\hosts',
+    /**
+     * Your workspace path, this could be your xamp or wamp path.
+     * for wamp it'll look like E:\wamp\www
+     */
+    'workspace_path' => 'D:\wamp\www',
+    /**
+     * local ip address.
+     */
+    'local_ip' => '192.168.1.6',
+];
